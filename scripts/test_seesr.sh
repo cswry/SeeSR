9f7275df18f11116ae552b8f5708a6d813c7d77c
cswry <22042244r@connect.polyu.hk>
2023-12-26 00:51:58 +0800

    update scripts

--- scripts/test_seesr.sh
+++ scripts/test_seesr.sh
@@ -1,13 +1,12 @@
 python test_seesr.py \
---pretrained_model_path /home/notebook/data/group/LowLevelLLM/models/diffusion_models/stable-diffusion-2-base \
---seesr_model_path preset/SeeSR_1225/seesr \
+--pretrained_model_path preset/models/stable-diffusion-2-base \
+--prompt None \
+--seesr_model_path preset/models/seesr \
 --ram_ft_path preset/models/DAPE.pth \
 --image_path preset/datasets/test_datasets \
---output_dir preset/datasets/output_1 \
---num_inference_steps 50 \
+--output_dir preset/datasets/output \
 --start_point lr \
---start_steps 999 \
---process_size 512 \
---upscale 1 \
---guidance_scale 5.5
+--num_inference_steps 50 \
+--guidance_scale 5.5 \
+--process_size 512 
 
--- scripts/train_seesr.sh
+++ scripts/train_seesr.sh
@@ -1,24 +1,26 @@
+# single gpu
 CUDA_VISIBLE_DEVICES="0," accelerate launch train_seesr.py \
 --pretrained_model_name_or_path="preset/models/stable-diffusion-2-base" \
 --output_dir="./experience/seesr" \
---root_folders 'your training dataset path' \
+--root_folders 'preset/datasets/training_datasets' \
 --ram_ft_path 'preset/models/DAPE.pth' \
 --enable_xformers_memory_efficient_attention \
 --mixed_precision="fp16" \
 --resolution=512 \
 --learning_rate=5e-5 \
---train_batch_size=2 \
+--train_batch_size=16 \
 --gradient_accumulation_steps=2 \
 --null_text_ratio=0.5 
 --dataloader_num_workers=0 \
 --checkpointing_steps=10000 
 
 
+# multi-gpus
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7," accelerate launch train_seesr.py \
 --pretrained_model_name_or_path="preset/models/stable-diffusion-2-base" \
 --output_dir="./experience/seesr" \
---root_folders 'your training dataset path' \
---ram_ft_path 'your dape model path' \
+--root_folders 'preset/datasets/training_datasets' \
+--ram_ft_path 'preset/models/DAPE.pth' \
 --enable_xformers_memory_efficient_attention \
 --mixed_precision="fp16" \
 --resolution=512 \
