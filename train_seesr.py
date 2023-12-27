cfbff2756e37a401379286aae9e06ee75272f914
cswry <22042244r@connect.polyu.hk>
2023-12-27 20:38:45 +0800

    Update train_seesr.py

--- train_seesr.py
+++ train_seesr.py
@@ -65,24 +65,6 @@ ram_transforms = transforms.Compose([
             transforms.Resize((384, 384)),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
-def load_state_dict_diffbirSwinIR(model: nn.Module, state_dict: Mapping[str, Any], strict: bool=False) -> None:
-    state_dict = state_dict.get("state_dict", state_dict)
-    
-    is_model_key_starts_with_module = list(model.state_dict().keys())[0].startswith("module.")
-    is_state_dict_key_starts_with_module = list(state_dict.keys())[0].startswith("module.")
-    
-    if (
-        is_model_key_starts_with_module and
-        (not is_state_dict_key_starts_with_module)
-    ):
-        state_dict = {f"module.{key}": value for key, value in state_dict.items()}
-    if (
-        (not is_model_key_starts_with_module) and
-        is_state_dict_key_starts_with_module
-    ):
-        state_dict = {key[len("module."):]: value for key, value in state_dict.items()}
-    
-    model.load_state_dict(state_dict, strict=strict)
 
 def image_grid(imgs, rows, cols):
     assert len(imgs) == rows * cols
